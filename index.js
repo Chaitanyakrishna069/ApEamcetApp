d965f26a4d19cfea0a058f5e40fa48c17ff9ea7d
chaitanya krishna <chaitanyakrishnach2000@gmail.com>
2022-01-28 18:30:49 +0530

    added script changes

--- index.js
+++ index.js
@@ -102,6 +102,7 @@ app.get("/",(req, res) => {
     data = keys
     res.render('moredetails',{data})
   });
-  app.listen(3000, () => {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
     console.log("I am listening");
   });
